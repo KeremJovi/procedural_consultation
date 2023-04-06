7194a1b878cdc3e9a922c3e524b49f1ed9570777
KeremJovi <joviellysudre@hotmail.com>
2023-04-06 00:45:50 -0300

    feat: deploying components to App root file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import Form from './components/Form';
+import Card from './components/Card';
 
 function App() {
-  return (
-    <div className="App" />
-  );
+	return (
+		<div className="App">
+			<Form />
+			<Card
+				numProcess="5555555555555555555555"
+				court="TJSP"
+				description="Assim, você pode garantir que o texto seja apresentado da melhor maneira possível, de acordo com o tamanho da tela."
+			/>
+		</div>
+	);
 }
 
 export default App;
