210a28aaa20b690e4eaeb3eb1dc4fae515df30f9
KeremJovi <joviellysudre@hotmail.com>
2023-04-09 12:38:11 -0300

    refactor: added routes for each page created

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Search from './pages/Search';
+import Details from './pages/details';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="/details" element={<Details />} />
+			<Route path="/search" element={<Search />} />
+		</Routes>
+	</BrowserRouter>
 );
