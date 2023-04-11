9f89ef09c8d3039dbf42658588a0468125a0c3a6
KeremJovi <joviellysudre@hotmail.com>
2023-04-11 09:23:24 -0300

    refactor: updated location of routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Search from './pages/Search';
-import Details from './pages/details';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />} />
-			<Route path="/details" element={<Details />} />
-			<Route path="/search" element={<Search />} />
-		</Routes>
-	</BrowserRouter>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
